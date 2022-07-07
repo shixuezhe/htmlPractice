4fe0c70e480825b50debbeb320bacfeaea0dcf89
shixuezhe <1159513993@qq.com>
2022-07-07 22:37:06 +0800

    1

---
+++ card_swiper/card.js
@@ -0,0 +1,87 @@
+class Card {
+  constructor({
+    imageUrl,
+    onDismiss,
+    onLike,
+    onDislike
+  }) {
+    this.imageUrl = imageUrl;
+    this.onDismiss = onDismiss;
+    this.onLike = onLike;
+    this.onDislike = onDislike;
+    this.#init();
+  }
+
+  // private properties
+  #startPoint;
+  #offsetX;
+  #offsetY;
+
+  #init = () => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const img = document.createElement('img');
+    img.src = this.imageUrl;
+    card.append(img);
+    this.element = card;
+    this.#listenToMouseEvents();
+  }
+
+  #listenToMouseEvents = () => {
+    this.element.addEventListener('mousedown', (e) => {
+      const { clientX, clientY } = e;
+      this.#startPoint = { x: clientX, y: clientY }
+      document.addEventListener('mousemove', this.#handleMouseMove);
+      this.element.style.transition = '';
+    });
+
+    document.addEventListener('mouseup', this.#handleMoveUp);
+
+    // prevent card from being dragged
+    this.element.addEventListener('dragstart', (e) => {
+      e.preventDefault();
+    });
+  }
+
+  #handleMouseMove = (e) => {
+    e.preventDefault();
+    if (!this.#startPoint) return;
+    const { clientX, clientY } = e;
+    this.#offsetX = clientX - this.#startPoint.x;
+    this.#offsetY = clientY - this.#startPoint.y;
+    const rotate = this.#offsetX * 0.1;
+    this.element.style.transform = `translate(${this.#offsetX}px, ${this.#offsetY}px) rotate(${rotate}deg)`;
+    // dismiss card
+    if (Math.abs(this.#offsetX) > this.element.clientWidth * 0.7) {
+      this.#dismiss(this.#offsetX > 0 ? 1 : -1);
+    }
+  }
+
+  #handleMoveUp = () => {
+    this.#startPoint = null;
+    document.removeEventListener('mousemove', this.#handleMouseMove);
+    this.element.style.transition = 'transform 0.5s';
+    this.element.style.transform = '';
+  }
+
+  #dismiss = (direction) => {
+    this.#startPoint = null;
+    document.removeEventListener('mouseup', this.#handleMoveUp);
+    document.removeEventListener('mousemove', this.#handleMouseMove);
+    this.element.style.transition = 'transform 1s';
+    this.element.style.transform = `translate(${direction * window.innerWidth}px, ${this.#offsetY}px) rotate(${90 * direction}deg)`;
+    this.element.classList.add('dismissing');
+    setTimeout(() => {
+      this.element.remove();
+    }, 1000);
+    if (typeof this.onDismiss === 'function') {
+      this.onDismiss();
+    }
+    if (typeof this.onLike === 'function' && direction === 1) {
+      this.onLike();
+    }
+    if (typeof this.onDislike === 'function' && direction === -1) {
+      this.onDislike();
+    }
+  }
+}
