9a1dc5c476014b823a08874b7dd3ff72e8683081
shixuezhe <1159513993@qq.com>
2022-07-07 22:03:38 +0800

    1

--- fixedNav_taobao/fixedNav_taobao.js
+++ fixedNav_taobao/fixedNav_taobao.js
@@ -1,1 +1,23 @@
+var container = document.querySelector(".container");
+var title = document.querySelector(".title");
+var content = document.querySelector(".content");
+var right_nav = document.querySelector(".right_nav");
+var toTop = document.querySelector(".toTop");
+var right_nav_Y = right_nav.offsetTop;
+var contentY = content.offsetTop;
 
+document.addEventListener('scroll', function(e){
+    if(window.scrollY >= contentY) {
+        right_nav.style.position = 'fixed';
+        right_nav.style.top = right_nav_Y - contentY + 'px';
+        toTop.style.display = 'block';
+    }else {
+        right_nav.style.position = 'absolute';
+        right_nav.style.top = '300px';
+        toTop.style.display = 'none';
+    }
+})
+
+toTop.addEventListener('click', function(e){
+    window.scrollTo(0, 0)
+})
